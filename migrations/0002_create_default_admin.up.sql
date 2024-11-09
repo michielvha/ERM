4bdcbb6f21e8892a31cb3f4f773d9b09d06bb712
DL6544 <DL6544@engie.com>
2024-11-09 22:08:04 +0100

    trying to provision default admin user into db.

--- migrations/0002_create_default_admin.up.sql
+++ migrations/0002_create_default_admin.up.sql
@@ -1,11 +1,3 @@
---INSERT INTO users (username, password, role, created_at)
---VALUES (
---    'admin',
---    crypt('secure_admin_password', gen_salt('bf')), -- Replace with a secure hashed password
---    'admin',
---    NOW()
---)
---ON CONFLICT (username) DO NOTHING; -- Prevents errors if the admin user already exists
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM users WHERE username = 'admin') THEN
