2635e0dfe8eec7c9f9f298c587cb24752daff061
DL6544 <DL6544@engie.com>
2024-11-09 21:50:00 +0100

    trying to provision default admin user into db.

---
+++ docs/troubleshooting.md
@@ -0,0 +1,32 @@
+# Troubleshooting Guide
+
+Details on how to troubleshoot the API.
+
+## Linux environment
+
+1. **Run the server**:
+   ```bash
+   go get 
+   go run main.go
+   ```
+
+2. **Login to get a JWT token**:
+   ```bash
+   curl -X POST http://localhost:8080/login -d '{"username": "admin", "password": "secure_admin_password"}' -H "Content-Type: application/json"
+   ```
+
+
+3. **Access the protected route**:
+   ```bash
+   curl -H "Authorization: Bearer <your-jwt-token>" http://localhost:8080/protected
+   ```
+
+## Windows environment
+
+**Powershell Example**:
+   ````powershell
+   $response = Invoke-RestMethod -Method Post -Uri http://localhost:8080/login -Body '{"username": "admin", "password": "secure_admin_password"}' -ContentType "application/json"
+   $token = $response.token
+   $token
+   Invoke-RestMethod -Method Get -Uri http://localhost:8080/protected -Headers @{ Authorization = "Bearer $token" }
+   ````
--- migrations/0002_create_default_admin.up.sql
+++ migrations/0002_create_default_admin.up.sql
@@ -1,8 +1,21 @@
-INSERT INTO users (username, password, role, created_at)
-VALUES (
-    'admin',
-    crypt('secure_admin_password', gen_salt('bf')), -- Replace with a secure hashed password
-    'admin',
-    NOW()
-)
+--INSERT INTO users (username, password, role, created_at)
+--VALUES (
+--    'admin',
+--    crypt('secure_admin_password', gen_salt('bf')), -- Replace with a secure hashed password
+--    'admin',
+--    NOW()
+--)
+--ON CONFLICT (username) DO NOTHING; -- Prevents errors if the admin user already exists
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM users WHERE username = 'admin') THEN
+        INSERT INTO users (username, password, role, created_at)
+        VALUES (
+            'admin',
+            crypt(current_setting('ADMIN_PASSWORD'), gen_salt('bf')),
+            'admin',
+            NOW()
+        );
+    END IF;
+END $$;
 ON CONFLICT (username) DO NOTHING; -- Prevents errors if the admin user already exists
